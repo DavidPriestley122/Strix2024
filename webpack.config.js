e5fb6d97d40018acde9295b900793c02d2857ad2
David Priestley <DavidMPriestley@aol.com>
2024-05-06 15:12:41 +0100

    First time working with modules

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
+export default {
+  mode: 'development', // Set the mode to 'development' or 'production'
+  entry: './main.js', // Update the entry point to './main.js'
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  resolve: {
+    extensions: ['.js'],
+    modules:['node_modules'],
+    alias: {
+      '@babylonjs/gui': path.resolve(__dirname, 'node_modules/@babylonjs/gui'),
+    },
+  },
+};
